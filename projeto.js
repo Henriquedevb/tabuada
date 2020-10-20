b330fbd2c1cc0fb9112f6fe250f3d345ac350740
Henrique fontelene <66540402+Henriquedevb@users.noreply.github.com>
2020-10-20 19:10:52 -0300

    Add files via upload

---
+++ projeto.js
@@ -0,0 +1,20 @@
+function tabuada(){
+    let num = document.getElementById('txtn');
+    let tab = document.getElementById('tabuadat');
+    
+    if(num.value.length == 0) {
+        window.alert('[ERRO] - Digite um número para continuar.')
+    } else {
+        let n = Number(num.value);
+        let c = 1;
+        tab.innerHTML = ''
+
+        while(c<=10) {
+            let item = document.createElement('option');
+            item.text = `${n} X ${c} = ${n*c}`
+            tab.appendChild(item);
+            c++;
+        }
+
+    }
+}
